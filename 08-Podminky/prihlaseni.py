e9411556fe8ceee453cd13ade8aadeebffdb66a7
Jiří Pešík <jiri.pesik@outlook.cz>
2022-08-01 16:01:04 +0200

    Úprava řešení

--- 08-Podminky/prihlaseni.py
+++ 08-Podminky/prihlaseni.py
@@ -1,10 +1,11 @@
 uzivatelskeJmeno = input("Zadej uživatelské jméno: ")
 heslo = input("Zadej heslo: ")
-if heslo == "simsalabim":
-  vek = input("Zadej věk: ")
-  vek = int(vek)
-  if vek > 18:
-    print("Smíš vstoupit.")
-else:
+if heslo != "simsalabim":
   print("Vstup nepovolen!")
   exit()
+vek = input("Zadej věk: ")
+vek = int(vek)
+if vek > 18:
+  print("Smíš vstoupit.")
+else:
+    print("Vstup nepovolen")
